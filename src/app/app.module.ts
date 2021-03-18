c0a3bbb7744d85b7b43d89e05de12b20efa63bc5
KGDarshan <kdarshang@gmail.com>
2021-03-18 11:34:07 +0530

    ajapro angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderPartComponent } from './header-part/header-part.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPartComponent
   ],
   imports: [
     BrowserModule,
